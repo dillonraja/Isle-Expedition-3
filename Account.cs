8cd6cdbed45c7738735d8f03591aea21459be8db
dillonraja <59807824+dillonraja@users.noreply.github.com>
2020-02-02 19:34:58 -0800

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsleExpeditions
+{
+
+
+}
+/// <summary>
+/// 
+/// </summary>
+class Account
+{
+    private static int lastAccountNumber = 0;
+    #region Properties
+    /// <summary>
+    /// Unique account number
+    /// </summary>
+    public int AccountNumber { get; private set; }
+    public string AccountName { get; set; }
+    public decimal Balance { get; private set; }
+
+    public string EmailAddress { get; set; }
+    public DateTime CreatedDate { get; set; }
+    #endregion
+
+    #region Constructor
+    public Account()
+    {
+        //lastAccountNumber += 1;
+        AccountNumber = ++lastAccountNumber;
+        CreatedDate = DateTime.UtcNow;
+    }
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Deposit money into account
+    /// </summary>
+    /// <param name="amount">Amount to deposit</param>
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public decimal Withdraw(decimal amount)
+    {
+        Balance -= amount;
+        return Balance;
+    }
+    #endregion
+}
+
+   
+        /// <summary>
+        /// This is an enemy on these Expeditions that can occasionally give loot.
+        /// The user inputs how many of "Mogu", "Pirates", "Void Dragons", "Vrykrul", ect. they have defeated and are shown the percentage based chance of how likely thay are to obtain an item.
+        /// There are 3 inputs in which enemy unites and give values "Rare" enemies which are an added 1% chance, Elite enemies that are a .33% chance, and normal enemies which are a 0.05% chance added.
+        /// A thing to note here is all enemies should have the same given calculation no matter what kind of enemy, but differing enemies give differing loot rewards.
+        /// Overall equation should be ((A+(B*0.33)+(c*0.05)= Answer of % based chance of obtaining loot.
+        /// 
+        ///This information is public to the user and has no personal data.
+        /// </summary>
+        ///
+        /// 
+        ///
+        //Issue found that it is uncertain how to enable the code to do a multiplaction problems or if the computer follows the rule of PEMDAS. Will edit in future version
+
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*******************");
+        Console.WriteLine("Welcome to the Isle Expeditions Collection Guide!");
+
+        while (true)
+        {
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Create an account");
+            Console.WriteLine("2. Look at loot tables");
+            Console.WriteLine("3. Enemies list");
+            Console.Write("Select an option: ");
+            var option = Console.ReadLine();
+            switch (option)
+            {
+                case "0":
+                    Console.WriteLine("Thank you for visiting the bank!");
+                    return;
+                case "1":
+                    Console.Write("Account name: ");
+                    var accountName = Console.ReadLine();
+
+                    Console.WriteLine("Email Address: ");
+                    var emailAddress = Console.ReadLine();
+             
+
+                    Console.Write("Amount to deposit: ");
+                    var amount = Convert.ToDecimal(Console.ReadLine());
+
+                   
+
+
+                    break;
+                case "2":
+
+             
+                default:
+                    Console.WriteLine("Invalid option! Try again!");
+                    break;
+            }
+        }
+
+    }
+}
